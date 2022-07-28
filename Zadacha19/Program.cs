be36336fb79c35b9fe2c6f3a689fe3bd8621970e
aa-buraev@bk.ru <aa-buraev@bk.ru>
2022-07-28 21:20:28 +0300

    Добавили решение задачи №19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+int n = 5;
+void FillData(int[] Data)
+{
+    int Lenght = Data.Length;
+    int i = 0;
+    Console.WriteLine("Введите пятизначное число");
+    while (i < Lenght)
+        {
+            Data[i] = Convert.ToInt32(Console.ReadLine());
+            i++;
+        }
+}
+void Palindrom(int[] x)
+{
+    if(x[0]==x[4] && x[1]==x[3])
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Число:" +x[0]+x[1]+x[2]+x[3]+x[4] +" является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Число:" +x[0]+x[1]+x[2]+x[3]+x[4] +" не является палиндромом");
+    }
+}
+int[] Data  = new int[n];
+FillData(Data);
+Palindrom(Data);
