b0e7976820aa685840d645a7afb099165ff470fb
aa-buraev@bk.ru <aa-buraev@bk.ru>
2022-07-30 14:08:07 +0300

    Добавили решение задачи №23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+void ryad (int count)
+{
+    count = 1;
+    while(count<=N)
+    {
+        Console.Write(Math.Pow(count,3) +"  ");
+        count++;
+    }
+}
+ryad(N);
